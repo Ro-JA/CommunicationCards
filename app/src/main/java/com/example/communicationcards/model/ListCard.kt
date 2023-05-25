9fae6dc17d7aebed1a680bd0476ed54ed4cbaa63
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-05-25 22:28:12 +0900

    add object ListCard

--- app/src/main/java/com/example/communicationcards/model/ListCard.kt
+++ app/src/main/java/com/example/communicationcards/model/ListCard.kt
@@ -1,2 +1,12 @@
 package com.example.communicationcards.model
 
+import com.example.communicationcards.R
+
+object ListCard {
+    val cards = mutableListOf<InformationCard>(
+        InformationCard(
+            imageCard = R.drawable.i_want,
+            textCard = R.string.i_want_card
+        )
+    )
+}
